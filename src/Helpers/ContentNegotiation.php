91324c00d9c06b60346d1661e2e046f806118e01
Prince <infor@rockblings.com>
2025-01-07 17:05:05 +0100

    fixing issue with github action workflow

--- src/Helpers/ContentNegotiation.php
+++ src/Helpers/ContentNegotiation.php
@@ -20,7 +20,7 @@ final readonly class ContentNegotiation implements ContentNegotiationContract
      */
     public function type(string $acceptHeader): string
     {
-        if (Config::string('api-response.app.env') === 'testing') {
+        if (Config::string('api-response.app.env', 'testing') === 'testing') {
             return 'json';
         }
 
